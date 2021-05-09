6c312eb8190f072c1d4912f6a562c37e665934a5
= <=>
2021-05-09 14:42:51 +0100

    'auth_started'

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template,request,redirect, url_for
+from flask import Flask, render_template,request,redirect, url_for,session,flash
+from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
+app.secret_key = 'wdjrfbhibjksdsdzcx'
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
+app.permanent_session_lifetime = timedelta(hours=1)
+
+db = SQLAlchemy(app)
 
 @app.route("/",methods = ['POST','GET'])
 def home():
@@ -9,12 +19,38 @@ def home():
     else:
         return render_template('home.html')
 
-@app.route("/login")
+@app.route("/login",methods = ['POST','GET'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        session.permanent = True
+        username = request.form['username']
+        password = request.form['password']
+        session['username'] = username
+        session['password'] = password
+        return redirect(url_for('cfp'))
+    else:
+        if ('username' in session) and ('password' in session):
+            return redirect(url_for('cfp'))
+        return render_template('login.html')
 
 @app.route("/register")
 def register():
     return render_template('register.html')
+
+@app.route('/cfp')
+def cfp():
+    if ('username' in session) and ('password' in session):
+        return render_template('cfp.html')
+    else:
+        flash('Login First','error')
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session.pop('username',None)    
+    session.pop('password',None)
+    flash('You have been logged out','info')
+    return redirect(url_for('login'))
+
 if __name__ == "__main__":
     app.run(debug = True)
