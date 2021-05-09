bb7c9d47409c92a217280085bd4efca4997ebda5
= <=>
2021-05-09 08:29:59 +0100

    soon to start auth

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods = ['POST','GET'])
 def home():
-    return render_template('home.html')
+    if request.method == 'POST':
+        return redirect(url_for('login'))
+    else:
+        return render_template('home.html')
 
-# app.route("/login")
-# def login():
-#     pass
+@app.route("/login")
+def login():
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug = True)
